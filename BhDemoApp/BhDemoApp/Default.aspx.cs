7e543f83610082e7fe5b1e35637523639b265662
easinrobin <robinkhondokar@gmail.com>
2019-10-25 16:48:20 +0600

    initial commit

--- BhDemoApp/BhDemoApp/Default.aspx.cs
+++ BhDemoApp/BhDemoApp/Default.aspx.cs
@@ -11,7 +11,6 @@ namespace BhDemoApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
         }
     }
 }
--- BhDemoApp/BhDemoApp/Default.aspx.designer.cs
+++ BhDemoApp/BhDemoApp/Default.aspx.designer.cs
@@ -13,5 +13,6 @@ namespace BhDemoApp
 
     public partial class _Default
     {
+
     }
 }
